58620df8ef224bb7edf1a75f43ee0640735ca074
unknown <shivaniroy1507@gmail.com>
2022-11-28 02:43:55 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,20 @@
+//setup date
+const date = (document.getElementById(
+  "date"
+).innerHTML = new Date().getFullYear());
+// setup nav
+const navBtn = document.getElementById("nav-btn");
+const navbar = document.getElementById("navbar");
+const navClose = document.getElementById("nav-close");
+// show nav
+navBtn.addEventListener("click", () => {
+  navbar.classList.add("showNav");
+});
+// close nav
+navClose.addEventListener("click", () => {
+  navbar.classList.remove("showNav");
+});
+//setup date
+// const date = (document.getElementById("date").innerHTML = new Date().getFullYear());
+
+// document.getElementById('test').style.color='red';
